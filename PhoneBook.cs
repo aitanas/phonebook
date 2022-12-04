08da07ef3ff3becd90af3607e539182c374cc68f
Aitana Shough <aeshough@gmail.com>
2022-12-04 15:22:38 -0800

    add namespace and empty methods

---
+++ PhoneBook.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic; //must add to any program file that uses a dict
+
+class PhoneBook
+{
+  public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+  static void Main()
+  {
+
+  }
+
+  static void AddContact() 
+  {
+
+  }
+
+  static void LookUpContact()
+  {
+    
+  }
+}
